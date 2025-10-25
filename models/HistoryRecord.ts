132b79efd20c19bfd78aec39f90e835b4bad607a
= <hugo.beets2003@gmail.com>
2025-10-25 22:29:16 +0200

    added models

---
+++ models/HistoryRecord.ts
@@ -0,0 +1,9 @@
+export interface HistoryRecord {
+    recorded_at: string,
+    hashrate1m: number,
+    hashrate5m: number,
+    hashrate1h: number,
+    hashrate1d: number,
+    hashrate7d: number,
+    avg_weight: number
+}
