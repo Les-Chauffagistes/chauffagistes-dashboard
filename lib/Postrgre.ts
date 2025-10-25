a67d53198364ed6aea7cfee98bbad2ff0f06d9fe
Hugo <hugo.beets2003@gmail.com>
2025-10-25 22:28:17 +0200

    added postresql lib

---
+++ lib/Postrgre.ts
@@ -0,0 +1,10 @@
+// lib/db.ts
+import { Pool } from "pg";
+
+export const pool = new Pool({
+  host: process.env.PGHOST,
+  port: 5432,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
+});
