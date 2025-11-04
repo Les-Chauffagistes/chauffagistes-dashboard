961d065cbe7da6cc11512226b26a55b692974622
= <hugo.beets2003@gmail.com>
2025-11-04 11:18:54 +0100

    added api payloads

--- models/API Payloads/WorkerHistoryRecord.ts
+++ models/API Payloads/WorkerHistoryRecord.ts
@@ -1,9 +1,9 @@
-export interface HistoryRecord {
+export interface WorkerHistoryRecord {
     timestamp: string,
     avg_hashrate1m: number,
     avg_hashrate5m: number,
     avg_hashrate1h: number,
     avg_hashrate1d: number,
     avg_hashrate7d: number,
-    avg_weight: number
+    avg_weight: string
 }
