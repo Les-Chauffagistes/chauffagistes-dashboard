1be248a9af4b4a2a93645cf4c63633ded2ce14b8
Hugo <hugo.beets2003@gmail.com>
2025-11-24 11:47:00 +0100

    updated community address

--- src/app/constants/columns.ts
+++ src/app/constants/columns.ts
@@ -1,5 +1,5 @@
 export const HASHRATE_COLUMNS = ["hashrate1m", "hashrate5m", "hashrate1hr", "hashrate1d", "hashrate7d"] as const;
-export const COMMUNITY_POOL_ADDRESS = "bc1qh8ge36h2njrp2aqv5ddpyph4g22elzgkds52ae";
+export const COMMUNITY_POOL_ADDRESS = "bc1qqp9zq4an6nyzhcspz2xfmkcf8rj0p6w94a5gyeu2a7rghxjhnqqsvymz5m";
 
 export type HashrateColumn = typeof HASHRATE_COLUMNS[number];
 
