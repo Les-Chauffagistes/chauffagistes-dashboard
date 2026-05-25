f020367f4a8348205a4cea77f638da14d2fa14f2
swakraft <hugo.beets2003@gmail.com>
2026-05-25 18:16:08 +0200

    fix secret location

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,11 +5,11 @@ set -e
 # Injection des secrets
 DB_PASS=$(cat /run/secrets/heatboard_staging_db_password)
 export PGPASSWORD="${DB_PASS}"
-export DATABASE_URL=postgresql://${DB_USER}:${DB_PASS}@${DB_HOST}:5432/${DB_NAME}
-export POOL_TOKEN=$(cat /run/secrets/pool_token)
-export SESSION_PASSWORD=$(cat /run/secrets/session_password)
-export DISCORD_CLIENT_SECRET=$(cat /run/secrets/discord_client_secret)
-export NEXTAUTH_SECRET=$(cat /run/secrets/nextauth_secret)
+export DATABASE_URL="postgresql://postgres:${DB_PASS}@${DB_HOST}:5432/${DB_NAME}"
+export POOL_TOKEN=$(cat /run/secrets/heatboard_staging_pool_token)
+export SESSION_PASSWORD=$(cat /run/secrets/heatboard_staging_session_password)
+export DISCORD_CLIENT_SECRET=$(cat /run/secrets/heatboard_staging_discord_client_secret)
+export NEXTAUTH_SECRET=$(cat /run/secrets/heatboard_staging_nextauth_secret)
 
 cat > public/config.js << CONF
 window.__CONFIG__ = {
